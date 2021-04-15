c9ab423d7a318d8fa067f3eadbd7d9666830ee47
Askar <askarkanatbekovic@gmail.com>
2021-04-15 09:09:39 +0400

    ch

--- src/components/AddForm.js
+++ src/components/AddForm.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React from 'react';
 import { useForm } from "react-hook-form";
 const AddForm = ({addUser, setShowForm}) => {
     // const [user, setUser] = useState({name:'', phone:''})
